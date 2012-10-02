bde0bdaf650781aeae278b9494d2c02b8c963f4e
developer <lgoldste@thoughtworks.com>
2012-10-01 21:38:45 -0500

    beginning of moving to OO

--- lib/radar_dynamo.rb
+++ lib/radar_dynamo.rb
@@ -1,78 +1,17 @@
 require 'date'
 
 class RadarDynamo
-	attr_accessor :data_dir, :item_types, :recommendations
+	attr_accessor :data_dir, :recommendations, :items
 
 	def initialize data_dir
 		@data_dir = data_dir
-		@item_types = ["Languages", "Tools", "Techniques", "Platforms"]
 		@recommendations = ["Adopt", "Trial", "Assess", "Hold"]
-	end
-
-	def items_with_recs
-		get_filenames(data_dir).map { |filename| 
-			[get_data_from_file(filename), date_of(filename)]
-		}.map { |file_text, date|
-			get_items_with_recommendations(file_text, date)
-		}.flatten
-	end
-
-	def get_filenames data_dir
-		Dir.entries(data_dir).reject { |filename|
-			filename.match /^\..*/
-		}
-	end
-
-	def get_items data_dir
-		get_filenames(data_dir).map { |filename|
-			[get_data_from_file(filename), date_of(filename)]
-		}.map { |file_content, date|
-			get_items_from_string(file_content, date)
-		}.inject(:+)
-	end
-
-	def date_of filename
-		matcher = filename.match /(\d{4})-(\d{2})\.txt/
-		Date.new(matcher[1].to_i, matcher[2].to_i, 1)
-	end
-
-	def get_data_from_file filename
-			all_text_in_file = ""
-			File.open(data_dir + "/" + filename).each_line { |line| 
-				all_text_in_file += line
-			}
-			all_text_in_file
-	end
-
-
-	def get_items_from_string file_text, radar_date
-		most_recent_header = ""
-
-		file_text.split("\n").select { |item| 
-			!item.nil? 
-		}.map { |item| 
-			most_recent_header = item if (item_types.include?(item)) 
-			most_recent_header == item ? nil : tech_object(item_name(item), radar_date, most_recent_header, item_number(item))
-		}.compact.reject { |item|
-			item.keys.include? nil
-		}
-	end
-
-	def item_number item
-		regex = /(\d*)\..*/
-		matcher = item.match(regex)
-		(matcher.nil? ? nil : (item.match regex)[1] )
-	end
-
-	def item_name item
-		regex = /\d*\. (.*)/
-		matcher = item.match(regex)
-		(matcher.nil? ? nil : (item.match regex)[1] )
+		@items = Items.new(data_dir)
 	end
 
 	def get_recommendations file_text, radar_date
-		file_text.split("\n").map { |item|
-			line_components = item.split(" ").map { |component|
+		file_text.split("\n").map { |datum|
+			line_components = datum.split(" ").map { |component|
 																							component.split(",")
 																						}.flatten
 			current_recommendation = line_components.first
@@ -102,17 +41,12 @@ class RadarDynamo
 			rec_numbers = rec_number_and_date.first
 			rec_date = rec_number_and_date.last
 
-			get_items_for_date(rec_date).map { |item|
-				item_dates = item.values.first
-
-				item_dates.map { |hash_with_item_date_as_key| 
-					item_date = hash_with_item_date_as_key.first
-					item_number = hash_with_item_date_as_key.last["number"]
-
-					rec_items_with_matching_dates(item_date, rec_date, rec_numbers, item_number, item, rec_type)
-				}
+			items.for_date(rec_date).map { |item|
+				rec_items_with_matching_dates(item.date, rec_date, rec_numbers, item.number, item, rec_type)
 			}
-		}.flatten
+		}.flatten.map { |item|
+			item.to_json
+		}
 	end
 
 	def rec_items_with_matching_dates item_date, rec_date, rec_numbers, item_number, item, rec_type
@@ -128,25 +62,112 @@ class RadarDynamo
 	end
 
 	def add_recommendation_value_to_item item, rec_type, date
-		rec = {"recommendation" => rec_type}
-		item.first.last[date].merge! rec
+		item.add_rec rec_type
 		item
 	end
 
-	def get_items_for_date(date)
-		get_items(data_dir).select { |item|
-			dates_from_item = item.values.first.keys
-			dates_from_item.include? date
-		}
-	end
+	
 
-	def tech_object item_name, radar_date, most_recent_header, item_number
-		{ item_name => { 
-				radar_date => {
-					"category" => most_recent_header,
-					"number" => item_number
+	class Item
+    attr_reader :name, :date, :category, :number, :recommendation
+
+    def initialize name, date, category, number
+    	@name, @date, @category, @number = name, date, category, number
+    end
+
+    def to_json
+    	{ name => { 
+					date => {
+						"category" => category,
+						"number" => number,
+						"recommendation" => recommendation
+					}
 				}
 			}
-		}
-	end
+    end
+
+    def add_rec recommendation
+    	@recommendation = recommendation
+    end
+
+    def to_s
+    	"#{name} #{date} #{category} #{number} #{recommendation}"
+    end
+  end
+
+  class Items
+  	include Enumerable
+
+    attr_reader :items, :data_dir, :item_types
+
+    def initialize data_dir
+    	@data_dir = data_dir
+    	@items = get_items(data_dir)
+    	@item_types = ["Languages", "Tools", "Techniques", "Platforms"]
+    end
+
+    def get_items data_dir
+			get_filenames(data_dir).map { |filename|
+				[get_data_from_file(filename), date_of(filename)]
+			}.map { |file_content, date|
+				get_items_from_string(file_content, date)
+			}.inject(:+)
+		end
+
+		def with_recs
+			get_filenames(data_dir).map { |filename| 
+				[get_data_from_file(filename), date_of(filename)]
+			}.map { |file_text, date|
+				get_items_with_recommendations(file_text, date)
+			}.flatten
+		end
+
+		def date_of filename
+			matcher = filename.match /(\d{4})-(\d{2})\.txt/
+			Date.new(matcher[1].to_i, matcher[2].to_i, 1)
+		end
+
+		def get_filenames data_dir
+			Dir.entries(data_dir).reject { |filename|
+				filename.match /^\..*/
+			}
+		end
+
+		def get_data_from_file filename
+			all_text_in_file = ""
+			File.open(data_dir + "/" + filename).each_line { |line| 
+				all_text_in_file += line
+			}
+			all_text_in_file
+		end
+
+		def get_items_from_string file_text, radar_date
+			most_recent_header = ""
+
+			file_text.split("\n").map { |datum| 
+				most_recent_header = datum if (["Languages", "Tools", "Techniques", "Platforms"].include?(datum)) 
+				most_recent_header == datum ? nil : Item.new(item_name(datum), radar_date, most_recent_header, item_number(datum))
+			}.compact.reject { |item|
+				item.name.nil?
+			}
+		end
+
+		def for_date(date)
+			get_items(data_dir).select { |item|
+				item.date == date
+			}
+		end
+
+		def item_number datum
+			regex = /(\d*)\..*/
+			matcher = datum.match(regex)
+			(matcher.nil? ? nil : (datum.match regex)[1] )
+		end
+
+		def item_name datum
+			regex = /\d*\. (.*)/
+			matcher = datum.match(regex)
+			(matcher.nil? ? nil : (datum.match regex)[1] )
+		end
+  end
 end
