6463ddb7bffd21b605d244a8b3d801ff33bd8057
developer <lgoldste@thoughtworks.com>
2012-10-09 12:54:38 -0500

    WIP simplifying; tests pass but some are invalid now

--- lib/items.rb
+++ lib/items.rb
@@ -20,23 +20,24 @@ class Items
     with_recs.map {|item| item.to_csv}
   end
 
+  
+
+  def with_recs
+    add_recs_to_items(get_recommendations_in_dir)
+  end
+
+
+  # def get_items_from_string file_content, date
+  # end
+
+  # private
+  
   def get_recommendations_in_dir
     get_filenames.map { |filename|
       [Parser.new.get_data_from_file(@data_dir, filename), Parser.new.date_of(filename)]
     }.map { |file_content, date| 
       Parser.new.get_recommendations_from_string(file_content, date)
     }.flatten
-  end
-
-  def get_items_from_string file_text, radar_date
-    most_recent_header = ""
-
-    file_text.split("\n").map { |datum| 
-      most_recent_header = datum if (["Languages", "Tools", "Techniques", "Platforms"].include?(datum)) 
-      most_recent_header == datum ? nil : Item.new(Parser.new.item_name(datum), radar_date, most_recent_header, Parser.new.item_number(datum))
-    }.compact.reject { |item|
-      item.name.nil?
-    }
   end
 
   def add_recs_to_items *recommendations
@@ -50,36 +51,23 @@ class Items
     items
   end
 
-  def rec_item_numbers current_recommendation, line_components
-    line_components.delete(current_recommendation)
-
-    line_components.reject { |component|
-      recommendation_types.include? component
-    }.map { |range_string|
-      range = range_string.split("-")
-      (range.first.to_i..range.last.to_i).map {|number|
-        number.to_s
-      }
-    }.flatten
+  def get_items
+    get_filenames.map { |filename|
+      file_content = Parser.new.get_data_from_file(@data_dir, filename)
+      date = Parser.new.date_of(filename)
+        most_recent_header = ""
+        file_content.split("\n").map { |datum| 
+        most_recent_header = datum if (["Languages", "Tools", "Techniques", "Platforms"].include?(datum)) 
+        most_recent_header == datum ? nil : Item.new(Parser.new.item_name(datum), date, most_recent_header, Parser.new.item_number(datum))
+      }.compact.reject { |item|
+        item.name.nil?
+      } 
+    }.inject(:+)
   end
 
   def get_filenames
     Dir.entries(@data_dir).reject { |filename|
       filename.match /^\..*/
     }
-  end
-
-  def with_recs
-    add_recs_to_items(get_recommendations_in_dir)
-  end
-
-  private
-
-  def get_items
-    get_filenames.map { |filename|
-      [Parser.new.get_data_from_file(@data_dir, filename), Parser.new.date_of(filename)]
-    }.map { |file_content, date|
-      get_items_from_string(file_content, date)
-    }.inject(:+)
   end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -29,39 +29,39 @@ describe Items do
     subject.to_json.should include json_ruby_adopt
   end
 
-  describe "#get_items_from_string" do
-    one_language = "Languages\n1. Ruby"
-    two_languages = "Languages\n1. Ruby\n2. Python"
-    two_item_types = "Languages\n1. Ruby\n\nTools\n14. Subversion"
-    two_item_types_one_rec = "Adopt 1\n\nLanguages\n1. Ruby\n\nTools\n2. Puppet"
-    ruby_item = Item.new("Ruby", radar_date, "Languages", "1")
-    python_item = Item.new("Python", radar_date, "Languages", "2") 
-    tools_item = Item.new("Subversion", radar_date, "Tools", "14")
+  # describe "#get_items_from_string" do
+  #   one_language = "Languages\n1. Ruby"
+  #   two_languages = "Languages\n1. Ruby\n2. Python"
+  #   two_item_types = "Languages\n1. Ruby\n\nTools\n14. Subversion"
+  #   two_item_types_one_rec = "Adopt 1\n\nLanguages\n1. Ruby\n\nTools\n2. Puppet"
+  #   ruby_item = Item.new("Ruby", radar_date, "Languages", "1")
+  #   python_item = Item.new("Python", radar_date, "Languages", "2") 
+  #   tools_item = Item.new("Subversion", radar_date, "Tools", "14")
 
-    it 'knows whether item is a language' do
-      subject.get_items_from_string(one_language, radar_date).should include ruby_item
-    end
+  #   it 'knows whether item is a language' do
+  #     subject.get_items_from_string(one_language, radar_date).should include ruby_item
+  #   end
 
-    it 'can see two items of the same type in a file' do
-      items = subject.get_items_from_string(two_languages, radar_date)
-      items.should include ruby_item
-      items.should include python_item
-    end
+  #   it 'can see two items of the same type in a file' do
+  #     items = subject.get_items_from_string(two_languages, radar_date)
+  #     items.should include ruby_item
+  #     items.should include python_item
+  #   end
 
-    it 'can see two kinds of item' do
-      items = subject.get_items_from_string(two_item_types, radar_date)
-      items.should include tools_item
-      items.should include ruby_item
-    end
+  #   it 'can see two kinds of item' do
+  #     items = subject.get_items_from_string(two_item_types, radar_date)
+  #     items.should include tools_item
+  #     items.should include ruby_item
+  #   end
 
-    it 'can compose an item with spaces in the name' do
-      subject.get_items_from_string("Tools\n10. Visualization & metrics", radar_date).should include tools_item
-    end
+  #   it 'can compose an item with spaces in the name' do
+  #     subject.get_items_from_string("Tools\n10. Visualization & metrics", radar_date).should include tools_item
+  #   end
 
-    it "doesn't return items with nil key even when dealing with multiple file sections" do
-      subject.get_items_from_string(two_item_types_one_rec, radar_date).first.name.should_not == nil
-    end
-  end
+  #   it "doesn't return items with nil key even when dealing with multiple file sections" do
+  #     subject.get_items_from_string(two_item_types_one_rec, radar_date).first.name.should_not == nil
+  #   end
+  # end
 
   adopt_1 = Recommendation.new("1", "Adopt", radar_date)
   hold_2 = Recommendation.new("2", "Hold", radar_date)
