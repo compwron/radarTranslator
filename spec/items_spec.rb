9b379aa9d04ff20471fd3a268ced2bd92cfc4c87
developer <lgoldste@thoughtworks.com>
2012-10-10 15:08:54 -0500

    removing outdated dead tests

--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -19,49 +19,10 @@ describe Items do
     subject.items.size.should == 2
   end
 
-  # it 'sees all items with recs in a data dir (more than 1 file)' do
-  #   subject.with_recs.should include Item.new("Ruby", radar_date, "Languages", "1", "Adopt")
-  #   subject.with_recs.should include Item.new("Python", radar_date, "Languages", "2", "Adopt")
-  # end
-
   it 'sees json of all items with recs in a data dir (more than 1 file)' do
     json_ruby_adopt = {"Ruby"=>{radar_date =>{"category"=>"Languages", "number" => "1"}}}
     subject.to_json.should include json_ruby_adopt
   end
-
-  # describe "#get_items_from_string" do
-  #   one_language = "Languages\n1. Ruby"
-  #   two_languages = "Languages\n1. Ruby\n2. Python"
-  #   two_item_types = "Languages\n1. Ruby\n\nTools\n14. Subversion"
-  #   two_item_types_one_rec = "Adopt 1\n\nLanguages\n1. Ruby\n\nTools\n2. Puppet"
-  #   ruby_item = Item.new("Ruby", radar_date, "Languages", "1")
-  #   python_item = Item.new("Python", radar_date, "Languages", "2") 
-  #   tools_item = Item.new("Subversion", radar_date, "Tools", "14")
-
-  #   it 'knows whether item is a language' do
-  #     subject.get_items_from_string(one_language, radar_date).should include ruby_item
-  #   end
-
-  #   it 'can see two items of the same type in a file' do
-  #     items = subject.get_items_from_string(two_languages, radar_date)
-  #     items.should include ruby_item
-  #     items.should include python_item
-  #   end
-
-  #   it 'can see two kinds of item' do
-  #     items = subject.get_items_from_string(two_item_types, radar_date)
-  #     items.should include tools_item
-  #     items.should include ruby_item
-  #   end
-
-  #   it 'can compose an item with spaces in the name' do
-  #     subject.get_items_from_string("Tools\n10. Visualization & metrics", radar_date).should include tools_item
-  #   end
-
-  #   it "doesn't return items with nil key even when dealing with multiple file sections" do
-  #     subject.get_items_from_string(two_item_types_one_rec, radar_date).first.name.should_not == nil
-  #   end
-  # end
 
   adopt_1 = Recommendation.new("1", "Adopt", radar_date)
   hold_2 = Recommendation.new("2", "Hold", radar_date)
